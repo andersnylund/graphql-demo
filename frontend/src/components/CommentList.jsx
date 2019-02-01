c33f90ca942b9cdbdb4ed28418a9b374b2282893
Anders Nylund <anders.nylund@hiq.fi>
2019-02-01 15:32:34 +0200

    Minor UI improvements

--- frontend/src/components/CommentList.jsx
+++ frontend/src/components/CommentList.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { array } from 'prop-types';
+
 import { Li, Ul } from './PostList';
 
 const CommentList = ({ comments }) => (
-  <Ul>
-    {comments.map(comment => (
-      <Li key={comment.id}>{`#${comment.id} ${comment.text}`}</Li>
-    ))}
-  </Ul>
+  <div>
+    <h3>Comments</h3>
+    <Ul>
+      {comments.map(comment => (
+        <Li key={comment.id}>{comment.text}</Li>
+      ))}
+    </Ul>
+  </div>
 );
+
+CommentList.propTypes = {
+  comments: array.isRequired,
+};
 
 export default CommentList;
--- frontend/src/components/PostTitle.jsx
+++ frontend/src/components/PostTitle.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { string } from 'prop-types';
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  color: goldenrod;
+  text-shadow: 1px 1px 1px gray;
+`;
 
 const PostTitle = ({ title }) => {
-  return <div>{title}</div>;
+  return <H1>{title}</H1>;
+};
+
+PostTitle.propTypes = {
+  title: string.isRequired,
 };
 
 export default PostTitle;
